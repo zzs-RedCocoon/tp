e42f2780ae2d1ff4bc23a45fe18dd3a37417f799
Chao Yi-Ju <sketch.2048@gmail.com>
2023-03-10 11:27:58 +0800

    Add parser and ui classes

--- src/main/java/MovieMate.java
+++ src/main/java/MovieMate.java
@@ -4,10 +4,43 @@ import java.util.Scanner;
  *
  */
 public class MovieMate {
+    private WatchedList watchedList = new WatchedList();
+    private ToWatchList toWatchList = new ToWatchList();
     public static void main(String[] args) {
         System.out.println("Hello from Movie Mate!");
         System.out.println("What is your name?");
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine() + ", welcome to movie mate");
+        System.out.println("Please enter the command to proceed with MovieMate :))");
+        String userInput = inputCommand();
+        String[] commandTypeAndParams = Parser.parseCommand(userInput);
+        String commandType = commandTypeAndParams[0];
+        String commandArg = commandTypeAndParams[1];
+        while(!commandType.equals("exit")) {
+            switch (commandType) {
+            case "watched":
+                // add to watched list
+                break;
+            case "towatch":
+                // add to to-watch list
+                break;
+            default:
+                Ui.help();
+            }
+        }
+    }
+    /**
+     * Scan in the user input and trim extra white space.
+     * If there is no input, continue to scan the next line for input.
+     * @return The string that the user entered
+     */
+    private static String inputCommand() {
+        Scanner scan = new Scanner(System.in);
+        String s;
+        s = scan.nextLine();
+        while (s.trim().isEmpty() || s.trim().charAt(0) == '#') {
+            s = scan.nextLine();
+        }
+        return s;
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,12 @@
+
+/**
+ * Text UI of the application.
+ */
+public class Ui {
+    public static void help() {
+        System.out.println("First time using Movie Mate?");
+        System.out.println("Here is the link to the user guide: ");
+        System.out.println("tbc");
+        System.out.println("Hope it helps, and have a nice day!");
+    }
+}
