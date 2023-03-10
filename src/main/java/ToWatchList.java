aace844f938d38f8b6a69bd9fb408c56329de9e7
Chao Yi-Ju <sketch.2048@gmail.com>
2023-03-10 13:28:39 +0800

    Remove unused import statement

--- src/main/java/ToWatchList.java
+++ src/main/java/ToWatchList.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 public class ToWatchList extends MovieList{
     public ToWatchList() {
--- src/main/java/WatchedList.java
+++ src/main/java/WatchedList.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 
 public class WatchedList extends MovieList{
     public WatchedList() {
