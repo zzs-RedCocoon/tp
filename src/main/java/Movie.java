d1a31bb2da036c5db5cd4e35c579b44e9a610348
EGGYPLANT\zhanhong <choongorilla@gmail.com>
2023-03-12 15:13:54 +0800

    Add methods

--- src/main/java/Movie.java
+++ src/main/java/Movie.java
@@ -5,7 +5,7 @@
  * Year of release, runtime/length (in minutes), and an array of up to 3 Genres.
  * Data is provided by IMDB.
  */
-public class Movie {
+public class Movie implements Comparable<Movie> {
     private final String titleID;
     private final String title;
     private final int year;
@@ -18,5 +18,39 @@ public class Movie {
         this.year = year;
         this.runTimeMinutes = runTimeMinutes;
         this.genres = genres;
+    }
+
+    /**
+     * Compare a movie to another movie. For the purposes of sorting.
+     * Sorting rules: Alphabetical title, then year.
+     * @param o the object to be compared.
+     * @return
+     */
+    @Override
+    public int compareTo(Movie o) {
+        return (this.getTitle().equals(o.getTitle())) ?
+                (this.getYear() - o.getYear())
+                : (this.getTitle().compareTo(o.getTitle()));
+    }
+
+    /* Getters all here below */
+    public String getTitleID() {
+        return titleID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getRunTimeMinutes() {
+        return runTimeMinutes;
+    }
+
+    public String[] getGenres() {
+        return genres;
     }
 }
