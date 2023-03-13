76de87b093ff62316f8c6676385418124ec490af
Zhan Hong <choongorilla@gmail.com>
2023-03-13 10:53:53 +0800

    Fix MovieMate main
    Previous design was causing infinite loop. LOL.

--- src/main/java/MovieMate.java
+++ src/main/java/MovieMate.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 /**
  * Movie Mate class is the main class that starts running the program.
@@ -9,11 +10,17 @@ public class MovieMate {
     private ToWatchList toWatchList = new ToWatchList();
     public static void main(String[] args) {
         Ui.showWelcomeMessage();
-        String userInput = inputCommand();
-        String[] commandTypeAndParams = Parser.parseCommand(userInput);
-        String commandType = commandTypeAndParams[0];
-        String commandArg = commandTypeAndParams[1];
-        while(!commandType.equals("exit")) {
+
+        // Initialise Scanner
+        Scanner scan = new Scanner(System.in);
+
+        while(true) {
+            // Get user input and parse it.
+            String userInput = inputCommand(scan);
+            String[] commandTypeAndParams = Parser.parseCommand(userInput);
+            String commandType = commandTypeAndParams[0];
+            String commandArg = commandTypeAndParams[1];
+
             switch (commandType) {
             case "watched":
                 // add to watched list
@@ -30,6 +37,12 @@ public class MovieMate {
             case "watchlist":
                 // list the to-wathc list
                 break;
+            case "exit":
+            case "bye":
+                // Fallthrough
+                // TODO: Extract this process and print message, save data, upon exit.
+                System.out.println("Bye!");
+                System.exit(0);
             default:
                 Ui.help();
                 break;
@@ -41,9 +54,8 @@ public class MovieMate {
      * If there is no input, continue to scan the next line for input.
      * @return The string that the user entered
      */
-    private static String inputCommand() {
-        Scanner scan = new Scanner(System.in);
-        String s;
+    private static String inputCommand(Scanner scan) {
+        String s = "";
         s = scan.nextLine();
         while (s.trim().isEmpty() || s.trim().charAt(0) == '#') {
             s = scan.nextLine();
