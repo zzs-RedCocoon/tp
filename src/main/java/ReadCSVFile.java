3a66055000b416337bf00d37d753587db4fb309d
EGGYPLANT\zhanhong <choongorilla@gmail.com>
2023-03-14 13:26:43 +0800

    Update ReadCSVFile
    Now can read full CSV Files

--- src/main/java/ReadCSVFile.java
+++ src/main/java/ReadCSVFile.java
@@ -1,19 +1,26 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ReadCSVFile {
+    // Split but ignore those in strings e.g. "Genre, Genre, Genre".
+    // Follow CSV convention.
+    // https://stackoverflow.com/questions/1757065/java-splitting-a-comma-separated-string-but-ignoring-commas-in-quotes
+    private static final String CSV_DELIM = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+    // We only want this many movies.
+    private static int MAX_LINES_READ = 10000;
+
     public static String[] find(String filePath, String movieName) {
-        String csvFile = "data/movies.csv";
+        String csvFile = filePath;
         String line = "";
-        String csvSplitBy = ",";
 
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
             Integer c = 0;
-            while (c < 10000 && (line = br.readLine()) != null) {
+            while (c < MAX_LINES_READ && (line = br.readLine()) != null) {
                 if (line.contains(movieName)) {
-                    String[] row = line.split(csvSplitBy);
+                    String[] row = line.split(CSV_DELIM, -1);
                     return row;
                 }
             }
@@ -21,6 +28,26 @@ public class ReadCSVFile {
             e.printStackTrace();
         }
         return null;
+    }
+
+    public static ArrayList<String[]> readEntireCSV(String filePath) {
+        String line = "";
+
+        ArrayList<String[]> output = new ArrayList<String[]>();
+        // Open file to read
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+
+            Integer numLinesRead = 0;
+            while (numLinesRead < MAX_LINES_READ && (line = br.readLine()) != null) {
+                String[] row = line.split(CSV_DELIM, -1);
+                output.add(row);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return output;
     }
 }
 
