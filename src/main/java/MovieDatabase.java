02b3340f78da92376ca12283aed69f155b523fcd
Chao Yi-Ju <sketch.2048@gmail.com>
2023-03-14 15:30:43 +0800

    Add find function

--- src/main/java/MovieDatabase.java
+++ src/main/java/MovieDatabase.java
@@ -1,11 +1,13 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class MovieDatabase {
 
     // Movie will be sorted, found by its String name.
-    private TreeMap<String, Movie> movieDatabase;
+    private static TreeMap<String, Movie> movieDatabase;
+    private static Integer MAX_RELEVANT_MOVIES = 5;
 
     public MovieDatabase(ArrayList<String[]> movieStrings) throws IOException {
         System.out.print("Loading movie database...");
@@ -29,5 +31,27 @@ public class MovieDatabase {
         }
         this.movieDatabase = movies;
         System.out.println(" Okay, movie database loaded.");
+    }
+
+    /**
+     * Find the movies that contain the movie name entered by the user and return them
+     *
+     * @param userInputMovieName The movie name entered by the user
+     * @return return an array list of movies with at most five movies
+     * whose names containing the user input movie name
+     */
+    public static ArrayList<Movie> find(String userInputMovieName) {
+        ArrayList<Movie> relevantMovies = new ArrayList<Movie>();
+        Integer movieCount = 0;
+        for (Map.Entry<String, Movie> entry: movieDatabase.entrySet()) {
+            if (entry.getKey().contains(userInputMovieName)) {
+                relevantMovies.add(entry.getValue());
+                movieCount += 1;
+            }
+            if (movieCount.equals(MAX_RELEVANT_MOVIES)) {
+                break;
+            }
+        }
+        return relevantMovies;
     }
 }
