78421bf7e4521740597a2441d5468de9370c74f4
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-03-26 11:08:19 +0800

    Recover Duke.

---
+++ src/test/java/seeedu/duke/DukeTest.java
@@ -0,0 +1,11 @@
+package seeedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class DukeTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
