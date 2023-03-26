9d2eb92a4ddd0c3c8f068e82f0f25beb2415f660
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-03-26 11:32:08 +0800

    Try to fix build problem.

---
+++ src/test/java/seedu/duke/DukeTest.java
@@ -0,0 +1,14 @@
+package seedu.duke;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+class DukeTest {
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+}
+
+
+
