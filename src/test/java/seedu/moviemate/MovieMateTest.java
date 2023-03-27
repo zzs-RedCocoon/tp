b93d2b8e8d14824298a6a098f2c8f07715536b52
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-03-27 22:12:24 +0800

    Change DukeTest to MovieMateTest.

--- src/test/java/seedu/moviemate/MovieMateTest.java
+++ src/test/java/seedu/moviemate/MovieMateTest.java
@@ -3,7 +3,7 @@ package seedu.moviemate;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+class MovieMateTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
