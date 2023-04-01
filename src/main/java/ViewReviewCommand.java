8948ec3ec8cd3381d4c1271f1e28d198f37311c5
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-04-02 00:33:25 +0800

    Deleted Duke and changed build.gradle. Packaged the files.

--- src/main/java/ViewReviewCommand.java
+++
@@ -1,8 +0,0 @@
-
-public class ViewReviewCommand extends Command {
-
-    @Override
-    public void execute(WatchedList watchedList, ToWatchList toWatchList, Ui ui, Storage storage) {
-        watchedList.viewReview(ui, watchedList);
-    }
-}
--- src/main/java/WatchListCommand.java
+++
@@ -1,9 +0,0 @@
-public class WatchListCommand extends Command {
-
-    @Override
-    public void execute(WatchedList watchedList, ToWatchList toWatchList, Ui ui, Storage storage) {
-        // list the towatch list
-        //toWatchList.list();
-        Ui.showListMessage(toWatchList);
-    }
-}
