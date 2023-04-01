bedb5c7a2f4e8c5d58f8f545cf017f77bb5fcbee
zzs-RedCocoon <xgps_0703zzs@126.com>
2023-04-02 00:12:58 +0800

    Delete duplicate List functions.

--- src/main/java/ListCommand.java
+++ src/main/java/ListCommand.java
@@ -4,7 +4,7 @@ public class ListCommand extends Command {
     @Override
     public void execute(WatchedList watchedList, ToWatchList toWatchList, Ui ui, Storage storage) {
         // list the watched list
-        watchedList.list();
+        // watchedList.list();
         Ui.showListMessage(watchedList);
     }
 }
--- src/main/java/WatchListCommand.java
+++ src/main/java/WatchListCommand.java
@@ -3,7 +3,7 @@ public class WatchListCommand extends Command {
     @Override
     public void execute(WatchedList watchedList, ToWatchList toWatchList, Ui ui, Storage storage) {
         // list the towatch list
-        toWatchList.list();
+        //toWatchList.list();
         Ui.showListMessage(toWatchList);
     }
 }
